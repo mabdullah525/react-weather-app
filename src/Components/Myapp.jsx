2de00292b78d373d3ea43da6ce2255eb3b5cb0ee
mabdullah525 <muhammadabdullahweb005@gmail.com>
2025-07-11 07:39:21 -0700

    style: enhance Myapp component layout and add styling in index.css

--- src/Components/Myapp.jsx
+++ src/Components/Myapp.jsx
@@ -1,20 +1,32 @@
 import React from 'react'
 
 const Myapp = () => {
-  return (
-    <>
-    <div className='container'>
-        <div className='input'>
-            <input type="text" placeholder='Enter City, Countary' />
+    return (
+        <>
+            <div className="header">
+                <div className="box">
+                    <h2>Search Weather</h2>
 
-        </div>
+                    <div className="flex items-center gap-2">
+                        <input
+                            type="text"
+                            placeholder="Enter City, Country"
+                        />
+                        <button>
+                            <i className="fa-solid fa-magnifying-glass"></i>
+                        </button>
+                    </div>
 
-    </div>
-    
-    
-    
-    </>
-  )
+                    <div className="mt-6 text-white text-center italic">
+                        {/* Future search result or info will be placed here */}
+                        Search for any city worldwide to get weather info.
+                    </div>
+                </div>
+            </div>
+
+
+        </>
+    )
 }
 
 export default Myapp
