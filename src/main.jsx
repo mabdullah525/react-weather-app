b5a49127b2eb9ffd1d875a8491c98f603923c522
mabdullah525 <muhammadabdullahweb005@gmail.com>
2025-07-11 07:08:51 -0700

    refactor: update title in index.html and restructure components

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import Myapp from './Components/Myapp'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Myapp />
   </StrictMode>,
 )
